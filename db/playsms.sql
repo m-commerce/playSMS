a4f04fe1c62325a66c76f49592b6eeb5f55a8e82
m-commerce <rob@m-commerce.net>
2013-06-10 16:34:28 +0200

    Update playsms.sql

--- db/playsms.sql
+++ db/playsms.sql
@@ -1306,17 +1306,17 @@ CREATE TABLE `playsms_tblUser` (
   `state` varchar(100) NOT NULL DEFAULT '',
   `country` int(11) NOT NULL DEFAULT '0',
   `zipcode` varchar(10) NOT NULL DEFAULT '',
-  `credit` double NOT NULL DEFAULT '0',
-  `datetime_timezone` varchar(30) NOT NULL DEFAULT '+0700',
+  `credit` double NOT NULL DEFAULT '10',
+  `datetime_timezone` varchar(30) NOT NULL DEFAULT '+0000',
   `language_module` varchar(10) NOT NULL DEFAULT 'en_US',
   `fwd_to_mobile` tinyint(4) NOT NULL DEFAULT '0',
-  `fwd_to_email` tinyint(4) NOT NULL DEFAULT '1',
+  `fwd_to_email` tinyint(4) NOT NULL DEFAULT '0',
   `fwd_to_inbox` tinyint(4) NOT NULL DEFAULT '1',
-  `replace_zero` varchar(5) NOT NULL DEFAULT '',
+  `replace_zero` varchar(5) NOT NULL DEFAULT '1',
   `plus_sign_remove` tinyint(4) NOT NULL DEFAULT '1',
-  `plus_sign_add` tinyint(4) NOT NULL DEFAULT '0',
+  `plus_sign_add` tinyint(4) NOT NULL DEFAULT '1',
   `send_as_unicode` tinyint(4) NOT NULL DEFAULT '0',
-  `local_length` tinyint(4) NOT NULL DEFAULT '9',
+  `local_length` tinyint(4) NOT NULL DEFAULT '10',
   `register_datetime` varchar(20) NOT NULL DEFAULT '0000-00-00 00:00:00',
   `lastupdate_datetime` varchar(20) NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`uid`)
@@ -1329,7 +1329,7 @@ CREATE TABLE `playsms_tblUser` (
 
 LOCK TABLES `playsms_tblUser` WRITE;
 /*!40000 ALTER TABLE `playsms_tblUser` DISABLE KEYS */;
-INSERT INTO `playsms_tblUser` VALUES (1332916885,1,2,'dd4aa3580e3de07858d812dcc094c32f','admin','21232f297a57a5a743894a0e4a801fc3','',0,'127.0.0.1, 192.168.*.*','Administrator','+62000000000','noreply@playsms.org','playSMS','@admin','','','',132,'',0,'+0700','en_US',0,1,1,'',1,0,0,0,'','');
+INSERT INTO `playsms_tblUser` VALUES (1332916885,1,2,'dd4aa3580e3de07858d812dcc094c32f','admin','21232f297a57a5a743894a0e4a801fc3','',0,'127.0.0.1, 192.168.*.*','Administrator','+44000000000','noreply@m-commerce.net','chillidrop','chillidrop','','','',108,'',10,'+0000','en_US',0,1,1,'',1,0,0,0,'','');
 /*!40000 ALTER TABLE `playsms_tblUser` ENABLE KEYS */;
 UNLOCK TABLES;
 
