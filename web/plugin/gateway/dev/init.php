0ade8e5750055e2f79239991f510163cd23c8b61
Rob <rob@leanelephant.com>
2013-06-11 17:58:40 +0100

    Removed unwanted themes & gateways

--- web/plugin/gateway/dev/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('_SECURE_') or die('Forbidden');
-
-include $apps_path['plug']."/gateway/dev/config.php";
-include $apps_path['plug']."/gateway/dev/fn.php";
-?>
--- web/plugin/gateway/gnokii/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('_SECURE_') or die('Forbidden');
-
-include $apps_path['plug']."/gateway/gnokii/config.php";
-include $apps_path['plug']."/gateway/gnokii/fn.php";
-?>
